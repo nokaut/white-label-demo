cc65959fb0dca50e56b319168ed3e32abe2d3fcc
jjuszkiewicz <jaroslaw.juszkiewicz@nokaut.pl>
2014-10-10 10:24:51 +0200

    main page

--- src/WL/AppBundle/Controller/DefaultController.php
+++ src/WL/AppBundle/Controller/DefaultController.php
@@ -5,11 +5,11 @@ namespace WL\AppBundle\Controller;
 use Nokaut\ApiKit\ClientApi\Rest\Fetch\ProductsFetch;
 use Nokaut\ApiKit\ClientApi\Rest\Query\ProductsQuery;
 use Nokaut\ApiKit\Collection\Products;
-use Nokaut\ApiKit\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use WL\AppBundle\Lib\CategoriesAllowed;
 use WL\AppBundle\Lib\Filter\PropertiesFilter;
-use WL\AppBundle\Lib\Pagination\Pagination;
+use WL\AppBundle\Lib\Repository\ProductsRepository;
+use WL\AppBundle\Lib\RepositoryFactory;
 use WL\AppBundle\Lib\Type\Breadcrumb;
 use WL\AppBundle\Lib\Repository\ProductsAsyncRepository;
 
@@ -22,39 +22,49 @@ class DefaultController extends Controller
         $breadcrumbs[] = new Breadcrumb("Strona główna");
 
 
-        /** @var ProductsAsyncRepository $productsAsyncRepo */
-        $productsAsyncRepo = $this->get('repo.products.async');
-        $products24Fetch = $this->fetchRandProducts($productsAsyncRepo);
-        $productsTop10Fetch = $productsAsyncRepo->fetchTopProducts();
-        $productsAsyncRepo->fetchAllAsync();
 
-        $products24 = $products24Fetch->getResult();
-        $productsTop10 = $productsTop10Fetch->getResult();
-        $this->filter($products24);
-        $this->filter($productsTop10);
+        /** @var RepositoryFactory $productsAsyncRepo */
+        $repositoryFactory = $this->get('repo.factory.cache.file');
+        $productsRepo = $repositoryFactory->getProductsAsyncRepository();
+
+        $productsGroupCategory = $productsFetchGroupCategory = array();
+
+        /** @var CategoriesAllowed $categoriesAllowed */
+        $categoriesAllowed = $this->get('categories.allowed');
+
+        foreach ($categoriesAllowed->getParametersCategories() as $allowedGroupCategories) {
+            $productsFetchGroupCategory[] = $this->fetchProducts($productsRepo, $allowedGroupCategories);
+        }
+
+        $productsRepo->fetchAllAsync();
+
+        foreach ($productsFetchGroupCategory as $productsFetch) {
+            /** @var ProductsFetch $productsFetch */
+            $products = $productsFetch->getResult();
+            $this->filter($products);
+            $productsGroupCategory[] = $products;
+        }
 
 
         return $this->render('WLAppBundle:Default:index.html.twig',
             array(
                 'breadcrumbs' => $breadcrumbs,
-                'products24' => $products24,
-                'productsTop10' => $productsTop10
+                'productsGroupCategory' => $productsGroupCategory
             ));
     }
 
     /**
      * @param ProductsAsyncRepository $productsAsyncRepo
+     * @param $categoriesIds
      * @return ProductsFetch
      */
-    protected function fetchRandProducts(ProductsAsyncRepository $productsAsyncRepo)
+    protected function fetchProducts(ProductsAsyncRepository $productsAsyncRepo, $categoriesIds)
     {
         $query = new ProductsQuery($this->container->getParameter('api_url'));
-        /** @var CategoriesAllowed $categoriesAllowed */
-        $categoriesAllowed = $this->get('categories.allowed');
-        $query->setCategoryIds($categoriesAllowed->getAllowedCategories());
+        $query->setCategoryIds($categoriesIds);
         $query->setFields(ProductsRepository::$fieldsForList);
-        $query->setOrder('random', 'asc');
-        $query->setLimit(24);
+        $query->setOrder('views', 'desc');
+        $query->setLimit(18);
         return $productsAsyncRepo->fetchProductsWithBestOfferByQuery($query);
     }
 
@@ -67,25 +77,5 @@ class DefaultController extends Controller
             $filterProperties = new PropertiesFilter();
             $filterProperties->filterProducts($products);
         }
-    }
-
-    /**
-     * @param ProductsFetch $productsFetch
-     * @return Pagination
-     */
-    protected function preparePagination($productsFetch)
-    {
-        /** @var Products|null $products */
-        $products = $productsFetch->getResult();
-        if (!$products) {
-            return new Pagination();
-        }
-        $pagination = new Pagination();
-        $pagination->setTotal($products->getMetadata()->getPaging()->getTotal());
-        $pagination->setCurrentPage($products->getMetadata()->getPaging()->getCurrent());
-        $pagination->setUrlTemplate(
-            $products->getMetadata()->getPaging()->getUrlTemplate()
-        );
-        return $pagination;
     }
 }
